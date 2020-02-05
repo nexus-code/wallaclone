73619bf5c1aaa94b45b62cdc525c7721f3960af7
Miguel Angel Cardenas <ma.cardenas@nexuscode.com>
2020-02-05 18:28:28 +0100

    Logged status -> Private Route!

--- front/src/components/PrivateRoute/PrivateRoute.js
+++ front/src/components/PrivateRoute/PrivateRoute.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 
+//thanks to 
+
 const PrivateRoute = ({ authorized, ...props }) =>
   authorized ? <Route {...props} /> : <Redirect to="/login" />;
 
