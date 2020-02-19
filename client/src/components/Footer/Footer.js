0a70152afb81b411784d5f6f3b31d3b9843dd220
Miguel Angel Cardenas <ma.cardenas@nexuscode.com>
2020-02-19 23:15:02 +0100

    Footer design

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import './footer.css'
 
 export default function Footer() {
 
     return (
 
-        <footer className="py-4 bg-dark text-white-50">
-            <div className="container text-center">
-                <small>Copyright &copy; Wallaclone </small>
-            </div>
+        <footer className="py-4 bg-dark text-white-50 text-center footer">
+            <p>Copyright &copy; Wallaclone </p>
         </footer>
     )
 };
