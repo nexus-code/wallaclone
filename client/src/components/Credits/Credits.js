86bfe2d072a130fdfc7fda23b819ea7982374a8c
Miguel Angel Cardenas <ma.cardenas@nexuscode.com>
2020-02-19 21:24:17 +0100

    Credits

--- client/src/components/Credits/Credits.js
+++ client/src/components/Credits/Credits.js
@@ -9,8 +9,8 @@ export default function Credits() {
     
     
     return <Canvas>
-        <div className="container mt-5 mb-5">
-            <h1>Miguel Ángel Cárdenas</h1>
+        <div className="container mt-5 mb-5">            
+            <h1><span>by</span>Miguel Ángel Cárdenas</h1>
             <a className="social" href="mailto:ma.cardenas@nexuscode.com" title="Contacto">ma.cardenas@nexuscode.com</a>
             <a className="social" href='https://www.linkedin.com/in/miguel-angel-cardenas/' title="Mi perfil" target="_blank">
                 <FontAwesomeIcon icon={faLinkedin} size="2x" />
@@ -18,7 +18,7 @@ export default function Credits() {
             <a className="social" href='https://github.com/nexus-code' title="Algunos trabajos" target="_blank">
                 <FontAwesomeIcon icon={faGithub} size="2x" />
             </a>
-            <a className="social" href="www.nexuscode.com" title="Más información en www.nexuscode.com" target="_blank">www.nexuscode.com</a>
+            <a className="social" href="http://www.nexuscode.com" title="Más información en nexuscode.com" target="_blank">www.nexuscode.com</a>
 
 
         </div>
