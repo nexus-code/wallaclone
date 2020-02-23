91fc32ca440113b92e86c1db0074eca7c12f1e87
Miguel Angel Cardenas <ma.cardenas@nexuscode.com>
2020-02-23 07:11:05 +0100

    Import material-ui Select from demo p11

--- client/src/components/AdvertEdit/AdvertEdit.js
+++ client/src/components/AdvertEdit/AdvertEdit.js
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import { useTranslation } from 'react-i18next';
 import { getAdvert } from '../../store/adverts/selectors';
+import Select from '@material-ui/core/Select';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
 import { useConfirm } from 'material-ui-confirm';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt, faCommentAlt } from "@fortawesome/free-regular-svg-icons";
-import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
+
 
 import AdvertListUser from '../AdvertListUser';
 
@@ -16,25 +20,12 @@ import './advertEdit.css';
 
 /**
  * 
- * Current user adverts handler: Allows CRUD methods
+ * Current user adverts handler: Do CRUD methods
  * List all his adverts quering by username
  * 
  */
 
- const tmpMaxAdverts = 50; //Dev: max adverts number to show
-
-const reactSelectStyles = {
-    control: styles => ({
-        ...styles,
-        paddingTop: '10px',
-        backgroundColor: '#fff',
-        borderColor: '#fff',
-        borderBottom: '1px solid rgba(0, 0, 0, .3)',
-        outline: 'none',
-        borderRadius: 0,
-        color: 'black',
-    }),
-}
+const tmpMaxAdverts = 50; //Dev: max adverts or current user number to show
 
 export default function AdvertEdit({
     user,
@@ -44,15 +35,16 @@ export default function AdvertEdit({
     tags,
     removeAdvert,
     loadAdverts,
-    match: {
-        params: { id },
-        path
-    },
+    match: { params: { id } },
 }) {
+
+    /*****************************************************
+     * initialization elements
+     */
+
     const { t } = useTranslation();
 
-    // Initialize
-    // Improve: Reset state.query
+    // Initialize filer string
     const queryDefault = {
         type: 'all',
         tag: 'all',
@@ -64,14 +56,53 @@ export default function AdvertEdit({
         sort: { created: 1 }
     };
 
-    const createAdvertLink = () => <Link to='../edit' className='createLink' >{t('Create advert')}</Link>;
 
-    const loadImage = image => <div>
-            {image && <img src={`${process.env.REACT_APP_API_IMAGES}xs-${image}`} alt='' />}
-            { !image && 'Without image!' }
-        </div>
+    const useStyles = makeStyles(theme => ({
+        formControl: {
+            margin: theme.spacing(1),
+            minWidth: 120,
+            maxWidth: 300,
+        },
+        chips: {
+            display: 'flex',
+            flexWrap: 'wrap',
+        },
+        chip: {
+            margin: 2,
+        },
+        noLabel: {
+            marginTop: theme.spacing(3),
+        },
+    }));
 
-    // componenDidMount. loads all adverts of the current user
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+        PaperProps: {
+            style: {
+                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250,
+            },
+        },
+    };
+
+    const names = [
+        'work',
+        'lifestyle',
+        'mobile',
+        'motor',
+    ];
+
+    function getStyles(name, personName, theme) {
+        return {
+            fontWeight: 400
+            // personName.indexOf(name) === -1
+            // ? theme.typography.fontWeightRegular
+            // : theme.typography.fontWeightMedium,
+        };
+    }
+
+    // componenDidMount. loads all the current user adverts
     useEffect(() => {
 
         const query = {
@@ -83,41 +114,49 @@ export default function AdvertEdit({
         advertQuerySet(query);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-    
+
     // componentWillUnmount. v1: Restore query by default
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect( () => { return async () => { advertQuerySet(queryDefault); }}, []);
-    
-    
+    useEffect(() => { return async () => { advertQuerySet(queryDefault); } }, []);
+
+    /*****************************************************
+     * Select advert methods
+     */
+
     const [advert, setAdvert] = useState({});
-    
+    const { register, handleSubmit, reset, errors } = useForm({ defaultValues: advert });
+
     useEffect(() => {
-        
-        adverts
-        &&
-        adverts.length
-        &&
-        setAdvert(getAdvert(adverts, id));
+
+        adverts && adverts.length && setAdvert(getAdvert(adverts, id));
     }, [adverts, id]);
-    
-          
+
+
+    // loads advert on form with reset() provided from react-hook-form
+    useEffect(() => {
+        if (advert) {
+            reset(advert);
+            setAdvertImage(advert.image);
+            console.log('advert.tags', advert.tags);
+            setAdvertTags(advert.tags);//*********************************************************************
+        }
+    }, [advert, reset]);
+
+
+    const createAdvertLink = () => <Link to='../edit' className='createLink' >{t('Create advert')}</Link>;
+
+    const loadImage = image => <div>
+        {image && <img src={`${process.env.REACT_APP_API_IMAGES}xs-${image}`} alt='' />}
+        {!image && 'Without image!'}
+    </div>
+
     const onEdit = id !== undefined;  // Edit when have an advert to did
     const pageTitle = onEdit ? 'Edit advert' : 'Create advert';
     const method = onEdit ? 'PUT' : 'POST';
-    
+
     const [imageFile, setImageFile] = useState();   //form field to upload image
     const [advertImage, setAdvertImage] = useState();   //image name stored in advert
     const [advertTags, setAdvertTags] = useState();   //tags stored in advert
-        
-     const { register, handleSubmit, reset, errors } = useForm({ defaultValues: advert });
-
-    // loads advert on form with reset function (provided from react-hook-form)
-    useEffect(() => {        
-        if(advert) {
-            reset(advert);
-            setAdvertImage(advert.image);
-        }
-    }, [advert, reset]);
 
 
     const reLoadAdverts = () => {
@@ -136,7 +175,7 @@ export default function AdvertEdit({
     const msg = t('This action is permanent! The advert will be removed');
     const handleRemove = () => {
         confirm({ title, description: msg })
-            .then(() => { 
+            .then(() => {
                 removeAdvert(id);
                 reLoadAdverts();
             });
@@ -155,12 +194,10 @@ export default function AdvertEdit({
         data.tags = advertTags;
         data.owner = user.id;
 
-        if( onEdit ){
+        if (onEdit) {
             data.id = id;
             data.image = advertImage;
         }
-
-
 
         // return saveAdvert(data, method);
         const newAdvert = saveAdvert(data, method);
@@ -168,9 +205,9 @@ export default function AdvertEdit({
         return newAdvert;
     }
 
-            
+
     const validator = (field, minLength, maxLength) => {
-        
+
         const validatorObj = {
             required: t(field) + ` ${t('is required')}`,
             // required: true,
@@ -186,8 +223,8 @@ export default function AdvertEdit({
 
         return validatorObj;
     };
-    
-    
+
+
     const handleImageChange = (e) => {
         e.persist();
         setImageFile(e.target.files[0]); // gets files object
@@ -195,13 +232,14 @@ export default function AdvertEdit({
     }
 
 
-    const handleSelectChange = options => {
+    const handleSelectTagsChange = options => {
 
         const t = options && options.map(opt => opt.value)
         setAdvertTags(t);
     }
 
-    const animatedComponents = makeAnimated(); //for Select tags
+
+    const handleChangeMultiple = ({ target: { value } }) => setAdvertTags(value)
 
 
     // componentWillUnmount. v1: Restore query by default with empty username
@@ -216,12 +254,25 @@ export default function AdvertEdit({
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    /**
+     * ***********************************************************************************************
+     */
+    const [personName, setPersonName] = React.useState([]);
+    const handleChange = event => {
+        setPersonName(event.target.value);
+    };
+
+    /**
+     * ***********************************************************************************************
+     */
+
+
 
     return (
         <Canvas>
             <div className="container edit-advert">
                 <h2>{t(pageTitle)}</h2>
-                { onEdit && createAdvertLink() }
+                {onEdit && createAdvertLink()}
                 <div className="edit-advert info">
                     {t('advert-edit-instructions')} <FontAwesomeIcon icon={faCommentAlt} />
                 </div>
@@ -231,9 +282,9 @@ export default function AdvertEdit({
                         <AdvertListUser />
                     </div>
                     <div className="edit-advert formContainer">
-                        { onEdit && <div className="remove"><Link to='#' onClick={handleRemove} className="remove"><FontAwesomeIcon icon={faTrashAlt} /> {t('Remove')}</Link></div> }
-                        { onEdit && loadImage(advertImage) }
- 
+                        {onEdit && <div className="remove"><Link to='#' onClick={handleRemove} className="remove"><FontAwesomeIcon icon={faTrashAlt} /> {t('Remove')}</Link></div>}
+                        {onEdit && loadImage(advertImage)}
+
                         <form onSubmit={handleSubmit(onSubmit)} >
                             <label>{t('Name')}</label>
                             <input
@@ -277,15 +328,50 @@ export default function AdvertEdit({
                             </select>
 
                             <label>{t('Tags')}</label>
-                            <Select
+                            {/* <Select
                                 isMulti
                                 closeMenuOnSelect={true}
                                 components={animatedComponents}
-                                onChange={handleSelectChange}
+                                onChange={handleSelectTagsChange}
                                 styles={reactSelectStyles}
                                 options={tags.map(tag => ({ label: tag, value: tag }))}
-                                defaultValue={ advertTags }
-                            />
+                                defaultValue={advertTags && advertTags.map(tag => ({ label: tag, value: tag })) }
+                            />                            
+                            */}
+                            {console.log('advert.tags', advert.tags)}
+                            {console.log('advertTags', advertTags)}
+                            <Select
+                                multiple
+                                name="tags"
+                                value={advertTags || []}
+                                onChange={handleChangeMultiple}
+                                renderValue={() => (
+                                    <div>
+                                        {advertTags && advertTags.map(value => (
+                                            <Chip
+                                                key={value}
+                                                size="small"
+                                                label={value}
+                                                className={`Ad__Tag Ad__Tag--small Ad__Tag--${value}`}
+                                            />
+                                        ))}
+                                    </div>
+                                )}
+                            >
+                                {tags &&
+                                    tags.map((value, key) => {
+                                        return (
+                                            <MenuItem key={key} value={value}>
+                                                <Chip
+                                                    key={key}
+                                                    size="small"
+                                                    label={value}
+                                                    className={`Ad__Tag Ad__Tag--small Ad__Tag--${value}`}
+                                                />
+                                            </MenuItem>
+                                        );
+                                    })}
+                            </Select>
                             {!advertTags && <p>mandatory</p>}
 
 
